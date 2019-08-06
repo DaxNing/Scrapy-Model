c3d15f1cec597289927fa66d08b51f29cd7d11aa
Dax <DaxNing@163.com>
2019-08-06 15:22:34 +0800

    Update cnnindonesia.py

--- cnnindonesia.py
+++ cnnindonesia.py
@@ -11,9 +11,9 @@ class Detick(scrapy.Spider):
 
     name = "cnnindonesia"
 
-    allowed_domains = ["cnnindonesia.com"]
-    start_urls=['https://www.cnnindonesia.com/gaya-hidup/20190726120843-255-415709/serba-serbi-air-mani-dan-aromanya']
-    i=992
+    allowed_domains = ["cnnindonesia.com"] #抓取限制范围
+    start_urls=['https://www.cnnindonesia.com/gaya-hidup/20190726120843-255-415709/serba-serbi-air-mani-dan-aromanya'] 
+    i=1  
 
 
     original_url = start_urls[0]
@@ -26,13 +26,16 @@ class Detick(scrapy.Spider):
 
         else:
             title = "No Title!"
+           
         content_list = response.xpath("/html/body/section[@id='content']/div[@class='container']/div[@class='l_content']/div[@class='content_detail']/div[@class='detail_wrap']/div[@id='detikdetailtext']//text()").extract()
         if len(content_list)!= 0:
-            content_list = [item.strip() for item in content_list]
-            content_ =" ".join(content_list)
+            content_list = [item.strip() for item in content_list] 
+            content_ =" ".join(content_list)        #对文章内容处理后合并
             content =content_.replace("\n","").encode("utf-8")
+            
         else:
             content = "No Content!"
+            
         print(self.i)
         dic["item_id"]=self.i
         dic["title"]=title
@@ -47,4 +50,4 @@ class Detick(scrapy.Spider):
         self.original_url=next_page
         if next_page is not None:
             next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+            yield scrapy.Request(next_page, callback=self.parse)    #跳转，dont_filter参数置为True可以使爬虫爬取重复网页
