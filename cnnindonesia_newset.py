89aea37dedd188d71297f2813864431321e323de
Dax <DaxNing@163.com>
2019-08-09 17:41:23 +0800

    Update cnnindonesia_newset.py

--- cnnindonesia_newset.py
+++ cnnindonesia_newset.py
@@ -4,19 +4,36 @@
 import scrapy
 import io
 import json
+import datetime
 import time
 import random
 
 class mySpider(scrapy.Spider):
     #分页爬取
-    
+
     name = "cnnindonesia_url"
 
+    start_date = '2018/11/01'
     # allowed_domains = ["cnnindonesia.com"]
-    start_urls=['https://www.cnnindonesia.com/indeks?date=2019/07/01']
+    start_urls=['https://www.cnnindonesia.com/indeks?date='+start_date+'&p=1&date='+start_date+'&kanal=2']
+
+
+
+    def get_before(self,date):
+        date_list = date.split("/")
+        now = date_list[0]+'-'+date_list[1]+'-'+date_list[2]
+        print(now)
+        d = datetime.datetime.strptime(now, '%Y-%m-%d')
+        before = d + datetime.timedelta(days=-1)
+        before = before.strftime('%Y/%m/%d')
+        print(before)
+        return before
+
 
     i=1
-    k=2 #页数
+    k=2
+
+    now = start_date
     def parse(self,response):
 
         s=1
@@ -31,15 +48,21 @@ class mySpider(scrapy.Spider):
             else:
                 break
 
-        next_page="https://www.cnnindonesia.com/indeks/"+str(self.k)+"?date=Semua%20Tgl&kanal=2"  #下一页链接
+        if s == 1 and self.now != "2017/01/01":  # 说明这页完全没有内容 or 不存在，跳转日期 #设置终止时间
+            self.now = self.get_before(self.now)
+            print("skipping date:%s" % (self.now))
+            with open("C:/Users/86135/Desktop/Spider_works/id_articles_from_lsscnnindonesia_day.txt", "a") as o:
+                o.write("skipping date: " + self.now + "\n")
+            self.k = 1
+
+
+        next_page="https://www.cnnindonesia.com/indeks?date="+self.now+"&p="+str(self.k)+"&date="+self.now+"&kanal=2"
         self.k+=1
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
 
     def choose_content(self,response):
-        #爬取每篇文章内容
-        
         dic = {}
         title_list = response.xpath(
             "/html/body/section[@id='content']/div[@class='container']/div[@class='l_content']/div[@class='content_detail']/h1[@class='title']//text()").extract()
@@ -63,7 +86,6 @@ class mySpider(scrapy.Spider):
         dic["content"] = content
         dic["original_url"] = response.url
         self.i += 1
-        with io.open("C:/Users/86135/Desktop/Spider_works/id_articles_from_newest_2.txt", "ab") as f:
+        with io.open("C:/Users/86135/Desktop/Spider_works/id_articles_from_newest_3.txt", "ab") as f:
             json.dump(dic, f)
             f.write("\n")
-
